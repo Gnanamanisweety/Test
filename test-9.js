88cbf1be61536bdeaa6eafa683c9ea1eff076efd
Gnanamanisweety <89681370+Gnanamanisweety@users.noreply.github.com>
2024-11-29 20:41:26 +0530

    Add files via upload

---
+++ test-9.js
@@ -0,0 +1,11 @@
+function reverseAndSortArray(arr){
+    arr.reverse()
+    console.log('Reversed Array: ')
+    console.log(arr)
+    arr.sort()
+    console.log('Sorted Array: ')
+    console.log(arr)
+
+}
+
+reverseAndSortArray(['y','a','i','t','r','s','j','b'])
